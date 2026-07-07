0b6341e7ae8d0615d52860099caa89fa40ed84fe
cym451010 <cym451010@naver.com>
2026-07-07 13:29:55 +0900

    네이티브 게임플레이 태그

---
+++ Source/Project_Warrior/Public/WarriorGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+//#include "../../../../../Program Files/Epic Games/UE_5.7/Engine/Source/Runtime/GameplayTags/Public/NativeGameplayTags.h"
+
+namespace NativeGameplayTags
+{
+	// Input Tags
+	PROJECT_WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	PROJECT_WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
